eb07882e0ba915535132e00c5cc04a6796195107
Nicolas Franck <Nicolas.Franck@ugent.be>
2012-06-22 17:05:46 +0200

    documenting

--- bagger-gui/src/main/java/gov/loc/repository/bagger/ui/util/XML.java
+++ bagger-gui/src/main/java/gov/loc/repository/bagger/ui/util/XML.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.Reader;
+import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.xml.XMLConstants;
@@ -12,12 +14,19 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
+import org.w3c.dom.DOMConfiguration;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.InputSource;
 
 /**
  *
@@ -27,11 +36,43 @@ public class XML {
 
     private static SchemaFactory sf = null;
     private static DocumentBuilderFactory dbf = null;
-    private static DocumentBuilder db = null;   
-    
+    private static DocumentBuilder db = null;
+    private static DOMImplementationRegistry registry = null;
+    private static DOMImplementationLS impl = null;
+
+    /*
+     * W3C DOM helper functions
+     */
+    public static DOMImplementationRegistry getDOMImplementationRegistry() throws Exception {
+        if(registry == null){
+            registry = DOMImplementationRegistry.newInstance();
+        }
+        return registry;
+    }
+
+    public static DOMImplementationLS getDOMImplementationLS() throws Exception {
+        if(impl == null){
+            impl = (DOMImplementationLS)(getDOMImplementationRegistry().getDOMImplementation("LS"));
+        }
+        return impl;
+    }
+    public static LSSerializer createLSSerializer() throws Exception{
+        return getDOMImplementationLS().createLSSerializer();
+    }
+    public static LSOutput createLSOutput(OutputStream out) throws Exception{
+        LSOutput lsout = getDOMImplementationLS().createLSOutput();
+        lsout.setByteStream(out);
+        return lsout;
+    }
+    public static LSOutput createLSOutput(Writer out) throws Exception{
+        LSOutput lsout = getDOMImplementationLS().createLSOutput();
+        lsout.setCharacterStream(out);
+        return lsout;
+    }
     public static DocumentBuilderFactory getDocumentBuilderFactory(){
         if(dbf == null){
             dbf = DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware(true);            
         }
         return dbf;
     }    
@@ -41,12 +82,20 @@ public class XML {
         }
         return db;
     }
+    public static Document createDocument() throws ParserConfigurationException{
+        return getDocumentBuilder().newDocument();
+    }
+    /*
+     * Schema helper functions
+     */
     public static SchemaFactory getSchemaFactory(){
         if(sf == null){
             sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         }
         return sf;
     }
+
+
     /*
      * check against schema (XSD or DTD)
      */
@@ -56,10 +105,6 @@ public class XML {
             new BufferedReader(new InputStreamReader(schemaURL.openStream()))
         );
     }
-    /*
-     * warning: do not reuse your inputstream or readers afterwards, for the parsers close them
-     * this will give a 'Bad File Descriptor'
-     */
     public static void validate(InputStream sourceInputStream,InputStream schemaInputStream) throws SAXException, IOException{
         StreamSource sourceStreamSource = new StreamSource(sourceInputStream);
         StreamSource schemaStreamSource = new StreamSource(schemaInputStream);
@@ -72,29 +117,69 @@ public class XML {
         Schema schema = getSchemaFactory().newSchema(schemaStreamSource);
         validate(sourceStreamSource,schema);
     }
+    public static void validate(Document doc,Schema schema) throws SAXException, IOException{
+        validate(new DOMSource(doc),schema);
+    }
     public static void validate(Source source,Schema schema) throws SAXException, IOException{
         Validator validator = schema.newValidator();
         validator.validate(source);
     }
+
+
     /*
      * check for well formedness
-     */
-    public static void validate(String sourceURL) throws ParserConfigurationException, MalformedURLException, IOException, SAXException{
-        validate(new URL(sourceURL));        
-    }
+     */    
     public static void validate(URL sourceURL) throws ParserConfigurationException, MalformedURLException, IOException, SAXException{
         validate(sourceURL.openStream());
     }
-    public static void validate(InputStream sourceIS) throws IOException, SAXException, ParserConfigurationException{
-        toDocument(sourceIS);
+    public static void validate(java.io.File sourceF) throws ParserConfigurationException, MalformedURLException, IOException, SAXException{
+        validate(new java.io.FileInputStream(sourceF));
     }
+    public static void validate(InputStream sourceIS) throws IOException, SAXException, ParserConfigurationException{
+        XMLToDocument(sourceIS);
+    }
+    public static void validate(Reader sourceR) throws IOException, SAXException, ParserConfigurationException{
+        XMLToDocument(sourceR);
+    }
+
     /*
      * convert XML input to W3C Document object
      */
-    public static Document toDocument(URL sourceURL) throws ParserConfigurationException, SAXException, IOException{
-        return toDocument(sourceURL.openStream());
+    public static Document XMLToDocument(java.io.File file) throws ParserConfigurationException, SAXException, IOException{
+        return XMLToDocument(new java.io.FileInputStream(file));
     }
-    public static Document toDocument(InputStream sourceIS) throws ParserConfigurationException, SAXException, IOException{
+    public static Document XMLToDocument(URL sourceURL) throws ParserConfigurationException, SAXException, IOException{
+        return XMLToDocument(sourceURL.openStream());
+    }
+    public static Document XMLToDocument(InputStream sourceIS) throws ParserConfigurationException, SAXException, IOException{
         return getDocumentBuilder().parse(sourceIS);
+    }
+    public static Document XMLToDocument(Reader sourceR) throws SAXException, ParserConfigurationException, IOException{
+        return getDocumentBuilder().parse(new InputSource(sourceR));
+    }
+
+    /*
+     * convert W3C Document object to XML
+     */
+    public static void DocumentToXML(Document doc,OutputStream out) throws Exception{
+        DocumentToXML(doc,createLSOutput(out));
+    }
+     public static void DocumentToXML(Document doc,OutputStream out,boolean pretty) throws Exception{
+        DocumentToXML(doc,createLSOutput(out),pretty);
+    }
+    public static void DocumentToXML(Document doc,Writer out) throws Exception{
+        DocumentToXML(doc,createLSOutput(out));
+    }
+    public static void DocumentToXML(Document doc,Writer out,boolean pretty) throws Exception{
+        DocumentToXML(doc,createLSOutput(out),pretty);
+    }
+    public static void DocumentToXML(Document doc,LSOutput lsout) throws Exception{
+        DocumentToXML(doc,lsout,false);
+    }    
+    public static void DocumentToXML(Document doc,LSOutput lsout,boolean pretty) throws Exception{
+        LSSerializer serializer = createLSSerializer();
+        DOMConfiguration conf = serializer.getDomConfig();                
+        conf.setParameter("format-pretty-print",new Boolean(pretty));
+        serializer.write(doc, lsout);
     }
 }
--- bagger-gui/src/main/java/gov/loc/repository/bagger/ui/util/XSLT.java
+++ bagger-gui/src/main/java/gov/loc/repository/bagger/ui/util/XSLT.java
@@ -10,35 +10,47 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import org.w3c.dom.Document;
 
 /**
  *
  * @author nicolas
  */
 public class XSLT {
-    private TransformerFactory tf;
+    private static TransformerFactory tf;
 
-    public TransformerFactory getTransformerFactory() {
+    public static TransformerFactory getTransformerFactory() {
         if(tf == null){
             tf = TransformerFactory.newInstance();
         }
         return tf;
     }
-    public void transform(URL sourceURL,URL xsltURL,OutputStream out) throws IOException, TransformerConfigurationException, TransformerException{
-        transform(
-                sourceURL.openStream(),
-                xsltURL.openStream(),
-                out
-        );       
+    public static void transform(URL sourceURL,URL xsltURL,OutputStream out) throws IOException, TransformerConfigurationException, TransformerException{
+        transform(sourceURL.openStream(),xsltURL.openStream(),out);       
     }
-    public void transform(InputStream sourceIS,InputStream xsltIS,OutputStream out) throws IOException, TransformerConfigurationException, TransformerException{
+    public static void transform(InputStream sourceIS,InputStream xsltIS,OutputStream out) throws IOException, TransformerConfigurationException, TransformerException{
         Source xmlSource = new StreamSource(sourceIS);
         Source xsltSource = new StreamSource(xsltIS);
         Result result = new StreamResult(out);        
         Transformer trans = getTransformerFactory().newTransformer(xsltSource);
         trans.transform(xmlSource,result);
     }
-
+    public static void transform(Document sourceD,InputStream xsltIS,Document outD) throws TransformerConfigurationException, TransformerException{               
+        Source xmlSource = new DOMSource(sourceD);
+        Source xsltSource = new StreamSource(xsltIS);
+        Result result = new DOMResult(outD);
+        Transformer trans = getTransformerFactory().newTransformer(xsltSource);
+        trans.transform(xmlSource,result);        
+    }
+    public static void transform(Document sourceD,Document xsltD,Document outD) throws TransformerConfigurationException, TransformerException{
+        Source xmlSource = new DOMSource(sourceD);
+        Source xsltSource = new DOMSource(xsltD);
+        Result result = new DOMResult(outD);
+        Transformer trans = getTransformerFactory().newTransformer(xsltSource);
+        trans.transform(xmlSource,result);
+    }
 }
